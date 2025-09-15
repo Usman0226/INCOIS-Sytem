324b59bb702d6048d85e00bccbb01e822d8d99b2
Usman0226 <chandanusmangani@gmail.com>
2025-09-15 17:57:12 +0530

    Frontend : Login & SignUp updates !, DB cluster setup

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/INCOIS')
+        await mongoose.connect('mongodb+srv://chandanusmangani_db_user:bWzcOUeQOhZ5YZbm@cluster1.vwunolx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1')
         console.log("DB connected !");
         
     } catch (error) {
-        console.log("DB connection failed !")       
+        console.log("DB connection failed !",error)       
     }
 }
 
