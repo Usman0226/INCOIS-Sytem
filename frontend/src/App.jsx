d999099f92163ccdf93527d08ac74cda8975e2f1
Usman0226 <chandanusmangani@gmail.com>
2025-10-02 15:18:14 +0530

    Extension fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignUp from "./components/SignUp.jsx";
 import Login from "./components/login.jsx"
-import HazardDashboard from "./components/HazardDashboard";
+import HazardDashboard from "./components/HazardDashboard.jsx";
 
 function App() {
   const [scientist, setScientist] = useState(null);
